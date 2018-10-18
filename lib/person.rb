60cf9013c410d8d5611b6e5a2c6fec0a8119cc36
Alexandria Walker-Wade <alexandriawalkerwade@gmail.com>
2018-10-18 03:27:47 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  
+  def talk
+    puts "Hello World!"
+  end
+end
+
+Alex = Person.new
+Alex.talk
+
+class Person
+  
+  def walk
+    puts "The Person is walking"
+  end
+end
+Alex = Person.new
+Alex.walk
